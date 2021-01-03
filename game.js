0111a157d4941e43897194827ab9233555811a63
Jasbir <mr.jasbirsingh96@gmail.com>
2021-01-03 23:29:13 +0530

    addedc bomb

--- game.js
+++ game.js
@@ -14,12 +14,12 @@ let config = {
     }
 };
 let game = new Phaser.Game(config);
-let player, cursors, platforms, coins;
+let player, cursors, platforms, coins, coronas, gameOver;
 gameScene.preload = function preload() {
     this.load.spritesheet("player", ".//assets//sprite//char.png", { frameWidth: 40, frameHeight: 45, });
     this.load.spritesheet("coin", ".//assets//sprite//coin.png", { frameWidth: 30, frameHeight: 30, });
     this.load.image("platform", ".//assets//sprite//platform.png");
-    
+    this.load.image("corona", ".//assets//sprite//corona.png");
 }
 gameScene.create = function create() {
     // scoreText = this.add.text(300, 200, "Hello Phaser", { fontSize: '32px', fill: "white" });
@@ -36,6 +36,9 @@ gameScene.create = function create() {
         repeat: 10,
         setXY: { x: 20, y: 0, stepX: 70 }
     });
+    coronas = this.physics.add.group();
+    createCorona();
+
     // **************************Animations***********************
     this.anims.create({
         key: 'left',
@@ -72,13 +75,18 @@ gameScene.create = function create() {
     })
     coins.playAnimation("shine");
     // player.anims.play("left");
+    // *************************Collision*****************
     player.setCollideWorldBounds(true);
     this.physics.add.collider(player, platforms);
     this.physics.add.collider(coins, platforms);
+    this.physics.add.collider(coronas, platforms);
+    this.physics.add.collider(player, coronas, hitCorona, null, this)
     this.physics.add.overlap(player, coins, collectCoin, null, this);
     cursors = this.input.keyboard.createCursorKeys();
 }
 gameScene.update = function update() {
+    if (gameOver == true)
+        return;
     if (cursors.left.isDown) {
         player.setVelocityX(-120);
         // ke liye collider add kar na padta hai
@@ -98,7 +106,7 @@ gameScene.update = function update() {
         player.setVelocityX(0);
         player.anims.play("idle", true);
     }
-    if (cursors.space.isDown) {
+    if (cursors.space.isDown&&player.body.touching.down) {
         player.setVelocityY(-300);
     }
 }
@@ -108,6 +116,19 @@ function collectCoin(player, coin) {
     if (coins.countActive(true) === 0) {
         coins.children.iterate((coin) => {
             coin.enableBody(true, coin.x, 0, true, true);
+            createCorona();
         });
     }
+}
+function createCorona() {
+    let x = player.x < 400 ? Phaser.Math.Between(400, 800) : Phaser.Math.Between(0, 400);
+    let corona = coronas.create(x, 16, "corona");
+    corona.setBounce(0.99);
+    corona.setCollideWorldBounds(true);
+    corona.setVelocity(Phaser.Math.Between(-200, 200), 20);
+}
+function hitCorona() {
+    this.physics.pause();
+    player.setTint(0xff0000);
+    gameOver = true;
 }
