063376a0e9037c7108626554b0a0b1028e61cec8
Jasbir <mr.jasbirsingh96@gmail.com>
2021-01-03 22:03:31 +0530

    added controls

--- game.js
+++ game.js
@@ -1,22 +1,26 @@
+let gameScene = new Phaser.Scene('Game');
 let config = {
     type: Phaser.AUTO,
     width: 800,
     height: 600,
     backgroundColor: 0x444444,
-    scene: {
-        preload: preload,
-        create: create,
-        update: update
+    scene: gameScene,
+    physics: {
+        default: 'arcade',
+        arcade: {
+            gravity: { y: 350 },
+            debug: false
+        }
     }
 };
 let game = new Phaser.Game(config);
-let scoreText, player;
-function preload() {
+let scoreText, player, cursors;
+gameScene.preload = function preload() {
     this.load.spritesheet("player", ".//assets//sprite//char.png", { frameWidth: 40, frameHeight: 45, });
 }
-function create() {
+gameScene.create = function create() {
     scoreText = this.add.text(300, 200, "Hello Phaser", { fontSize: '32px', fill: "white" });
-    player = this.add.sprite(250, 200, 'player');
+    player = this.physics.add.sprite(250, 200, 'player');
     player.setFrame(4);
     this.anims.create({
         key: 'left',
@@ -35,8 +39,24 @@ function create() {
         frames: [{ key: 'player', frame: 4 }],
         frameRate: 20
     });
-    player.anims.play("left");
+    // player.anims.play("left");
+    player.setCollideWorldBounds(true);
+    cursors = this.input.keyboard.createCursorKeys();
+    console.log(cursors);
 }
-function update() {
-    console.log("running update");
+gameScene.update = function update() {
+    if (cursors.left.isDown) {
+        player.setVelocityX(-120);
+        player.anims.play("left", true);
+    } else if (cursors.right.isDown) {
+        player.setVelocityX(120);
+        player.anims.play("right", true);
+    } else {
+        player.setVelocityX(0);
+        player.anims.play("idle", true);
+    }
+    if (cursors.space.isDown) {
+        player.setVelocityY(-300);
+
+    }
 }
